3fecc146946fd4eac85c5af7573969401978ce60
shivam2575 <sk2575@gmail.com>
2025-05-14 13:45:20 +0530

    added skills section

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import About from "./components/About";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
+import Skill from "./components/Skill";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <main>
         <Hero />
         <About />
+        <Skill />
       </main>
     </>
   );
