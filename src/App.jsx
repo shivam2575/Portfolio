38e11cb5c6612ee703e15c0c3acdab55d66966a9
shivam2575 <sk2575@gmail.com>
2025-05-14 18:25:56 +0530

    added reviews

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import About from "./components/About";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
+import Review from "./components/Review";
 import Skill from "./components/Skill";
 import Work from "./components/Work";
 
@@ -13,6 +14,7 @@ function App() {
         <About />
         <Skill />
         <Work />
+        <Review />
       </main>
     </>
   );
