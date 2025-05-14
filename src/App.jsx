0927d3a725a87c4e2915a4f585063789fb01f922
shivam2575 <sk2575@gmail.com>
2025-05-14 15:37:54 +0530

    added portfolio section

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import About from "./components/About";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
 import Skill from "./components/Skill";
+import Work from "./components/Work";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Hero />
         <About />
         <Skill />
+        <Work />
       </main>
     </>
   );
